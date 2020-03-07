603a1b220ad8137308371557183fff8134c1db2f
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-03-07 13:04:24 +0530

    [BUILD] Common Classes for project

--- src/views/container/App.js
+++ src/views/container/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <center>Hello Im Using React</center>
+      <center className="style-check">Hello Im Using React</center>
     </div>
   );
 }
