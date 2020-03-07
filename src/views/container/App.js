40c862517ef53290f9d693ffb7f6bf624d65b61a
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-03-07 13:11:21 +0530

    [SETUP] All project working setup completed 🔥

--- src/views/container/App.js
+++ src/views/container/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Dashboard from '../dashboard/dashboard';
 
 function App() {
   return (
     <div className="App">
-      <center className="style-check">Hello Im Using React</center>
+      {/* <center className="style-check">Hello Im Using React</center> */}
+      <Dashboard />
     </div>
   );
 }
---
+++ src/views/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export default class Dashboard extends Component{
+    render(){
+        return(<h1>
+            <center>
+                Hello REACT
+            </center>
+        </h1>)
+    }
+}
