37ffa4d17f8340afbb64c4d598be4d4b69a0c4e8
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-03-07 12:30:15 +0530

    [BUILD/ADD] Common Card Component and add fonts and body style

---
+++ src/views/config/constants.js
@@ -0,0 +1,5 @@
+export const Constant={
+    cardinal_color:'#C7222A',
+    box_shadow_color:'#CCC',
+    white_color:'#FFFFFF',
+}
---
+++ src/views/utils/card.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Constants from 'config/constants';
+
+export default class Card extends React.Component {
+  render() {
+    const cardStyle = {
+      boxShadow:
+        this.props.shadowColor && `${this.props.shadowColor} 0px 0px 20px 0px`,
+      backgroundColor: Constants.white_color,
+      borderRadius: this.props.borderRadius,
+      minWidth: this.props.minWidth,
+      borderBottom: this.props.bottomBorder
+        ? `${this.props.borderSize} solid ${this.props.borderColor}`
+        : 'none',
+      pointerEvents: this.props.pointerEvents
+    };
+
+    return (
+      <div className={this.props.className} style={cardStyle}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  className: PropTypes.string,
+  shadowColor: PropTypes.string,
+  borderSize: PropTypes.string,
+  borderColor: PropTypes.string,
+  borderRadius: PropTypes.string,
+  minWidth: PropTypes.string
+};
+
+Card.defaultProps = {
+  className: '',
+  shadowColor: Constants.box_shadow_color,
+  borderSize: '4px',
+  bottomBorder: true,
+  borderColor: Constants.cardinal_color,
+  borderRadius: '4px',
+  minWidth: '',
+  pointerEvents: 'auto'
+};
